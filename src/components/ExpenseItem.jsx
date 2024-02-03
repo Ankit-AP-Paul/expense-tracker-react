16bc7a426b15aaaf91a3ce8d61acb9748bd3a620
Ankit Paul <ankitpaul894@gmail.com>
2024-02-03 20:07:04 +0530

    updated ui

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,16 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
       <div className="expense_item_description">
         <div className="expense-item_price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
